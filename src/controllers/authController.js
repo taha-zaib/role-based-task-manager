0774391d5f71508bd7b41c956981c2b8b20cd7fc
dev <dev@gmail.com>
2026-05-18 14:55:34 +0500

    Build Login and JWT Authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
     try {
@@ -41,6 +42,59 @@ const registerUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid Email"
+            })
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            res.status(400).json({
+                message: "Invalid Password"
+            })
+        }
+
+        //generate jwt token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRES_IN
+            }
+        )
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
